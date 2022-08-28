17f2e56de9b1e794c6d11d3241ffa08a4eb7cf01
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2022-08-28 22:23:35 +0700

    Website buying and selling car

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import Header from "./component/common/head/Header.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./component/home/Home.js";
+import About from "./component/about/About.js";
+import CoursesHome from "./component/allcourses/CoursesHome.js";
+import Team from "./component/team/Team.js";
+import Price from "./component/pricing/Price.js";
+import Blog from "./component/blog/Blog.js";
+import Contact from "./component/contact/Contact.js";
+import Footer from "./component/common/footer/Footer.js";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/courses">
+            <CoursesHome />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route path="/pricing">
+            <Price />
+          </Route>
+          <Route path="/journal">
+            <Blog />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
